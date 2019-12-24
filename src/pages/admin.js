085dae93be0572ea07141d7a54ea3dac8fc1b284
Kyle Lemire <kyle@elykinnovation.com>
2019-12-24 10:51:25 -0600

    Adding admin page so route doesn't 404 before redirect

---
+++ src/pages/admin.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Admin = () => {
+  return (
+    <>
+      <h1>Sit tight, you are being redirected</h1>
+      <p>
+        If you are not redirected,{" "}
+        <a href="https://gatsby-wordpress.elykinnovation.com/wp-admin/">
+          reach the WordPress login manually
+        </a>
+        .
+      </p>
+    </>
+  )
+}
+
+export default Admin
